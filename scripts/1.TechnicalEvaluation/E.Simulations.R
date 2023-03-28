c33ca61529ce243f2927c8a600a5fed5ce0d93e5
Carlos Ruiz <yocra3@gmail.com>
2023-03-28 12:59:10 +0200

    Add Docker file and simulations

---
+++ scripts/1.TechnicalEvaluation/E.Simulations.R
@@ -0,0 +1,401 @@
+#'#################################################################################
+#'#################################################################################
+# Simulate epimutations based on GSE84727 data
+#'#################################################################################
+#'#################################################################################
+
+# Prepare data ####
+## Load libraries ####
+library(ramr)
+library(epimutacions)
+library(minfi)
+library(tidyverse)
+
+## Load data ####
+load("data/GSE84727.autosomic.filterAnnotatedProbes.withNA.GenomicRatioSet.Rdata")
+
+## Convert GRSet to ramr format
+data.ranges <- granges(gset)
+data.betas  <- getBeta(gset)
+sample.ids  <- colnames(gset)
+mcols(data.ranges) <- data.betas
+
+## Simulate data ####
+set.seed(27)
+
+# unique random AMRs
+amrs.unique_020 <-
+  simulateAMR(data.ranges, nsamples = 100, regions.per.sample = 10,
+              min.cpgs = 3, merge.window = 1000, dbeta = 0.20)
+
+amrs.unique_040 <-
+  simulateAMR(data.ranges, nsamples = 100, regions.per.sample = 10,
+              min.cpgs = 3, merge.window = 1000, dbeta = 0.40)
+
+# random noise outside of AMR regions
+noise_020 <-
+  simulateAMR(data.ranges, nsamples = 100, regions.per.sample = 10,
+              exclude.ranges = amrs.unique_020,
+              min.cpgs = 1, max.cpgs = 1, merge.window = 1, dbeta = 0.5)
+
+noise_040 <-
+  simulateAMR(data.ranges, nsamples = 100, regions.per.sample = 10,
+              exclude.ranges = amrs.unique_040,
+              min.cpgs = 1, max.cpgs = 1, merge.window = 1, dbeta = 0.5)
+
+
+sim.data020 <-
+  simulateData(data.ranges, nsamples = 100,
+               amr.ranges = c(amrs.unique_020, noise_020), cores = 2)
+
+save(sim.data020, amrs.unique_020, noise_020,
+     file = "results/simulations/sim020_ramr_format.Rdata")
+simGset020 <- makeGenomicRatioSetFromMatrix(data.matrix(mcols(sim.data020)))
+save(simGset020, file = "results/simulations/sim020_GRSet.Rdata")
+
+sim.data040 <-
+  simulateData(data.ranges, nsamples = 100,
+               amr.ranges = c(amrs.unique_040, noise_040), cores = 2)
+
+save(sim.data040, amrs.unique_040,  noise_040,
+     file = "results/simulations/sim040_ramr_format.Rdata")
+simGset040 <- makeGenomicRatioSetFromMatrix(data.matrix(mcols(sim.data040)))
+save(simGset040, file = "results/simulations/sim040_GRSet.Rdata")
+
+# Effect size ####
+## Run epimutacions methods using LOO ####
+epimut_res020 <- lapply(names(epi_parameters()), function(met){
+  epimutations_one_leave_out(simGset020, method = met)
+})
+names(epimut_res020) <- names(epi_parameters())
+
+
+epimut_res040 <- lapply(names(epi_parameters()), function(met){
+  epimutations_one_leave_out(simGset040, method = met)
+})
+names(epimut_res040) <- names(epi_parameters())
+save(epimut_res020, epimut_res040, file = "results/simulations/sim_epimut_res.Rdata")
+
+## Run RAMR ####
+ramr_res020 <- lapply(c("IQR", "beta", "wbeta"), function(met){
+  getAMR(sim.data020, ramr.method = met, min.cpgs = 3,
+         qval.cutoff = 1e-3,
+         merge.window = 1000, cores = 2)
+})
+
+ramr_res040 <- lapply(c("IQR", "beta", "wbeta"), function(met){
+  getAMR(sim.data040, ramr.method = met, min.cpgs = 3,
+         qval.cutoff = 1e-3,
+         merge.window = 1000, cores = 2)
+})
+names(ramr_res040) <- names(ramr_res020) <- c("ramr-IQR", "ramr-beta", "ramr-wbeta")
+save(ramr_res020, ramr_res040, file = "results/simulations/sim_ramr_res.Rdata")
+
+## Evaluate ####
+all.ranges <- getUniverse(sim.data020, min.cpgs = 3, merge.window = 1000)
+
+epimut_res020GR <- lapply(epimut_res020, function(x) {
+  GR <- makeGRangesFromDataFrame(x, keep.extra.columns = TRUE)
+  GR[seqnames(GR) != 0]
+})
+epimut_res040GR <- lapply(epimut_res040, function(x) {
+  GR <- makeGRangesFromDataFrame(x, keep.extra.columns = TRUE)
+  GR[seqnames(GR) != 0]
+})
+
+# true positives
+getValues <- function(res, epi_regs, all_ranges){
+  
+  
+  all_neg <- all_ranges[all_ranges %outside% epi_regs]
+  
+  if(length(res) == 0){
+    return(  c(TPu = 0,
+               TPn = 0, 
+               FP = 0, 
+               TN = length(all_neg),
+               FNu = length(unique_reg), 
+               FNn = length(nonunique_reg)
+            
+    ) )
+  } else {
+  c(TP = sum(sapply(unique(res$sample), function(x) sum(subset(res, sample == x) %over% subset(epi_regs, sample == x)))), 
+    FP = sum(res %outside% epi_regs),
+    TN = sum(all_neg %outside%  res),
+    FN = sum(sapply(unique(res$sample), function(x) sum(subset(epi_regs, sample == x) %outside% subset(res, sample == x))))
+  ) 
+  }
+}
+
+summary_tab20 <- sapply(c(ramr_res020, epimut_res020GR), 
+                        getValues, epi_regs = amrs.unique_020,
+                        all_ranges = all.ranges)
+
+summary_tab40 <- sapply(c(ramr_res040, epimut_res040GR), 
+                        getValues, epi_regs = amrs.unique_040,
+                        all_ranges = all.ranges)
+
+rmar_df <- rbind(summary_tab20 %>% 
+  t() %>%
+  data.frame() %>%
+  mutate(Method = rownames(.), 
+         Dataset = "0.20"),
+  summary_tab40 %>% 
+    t() %>%
+    data.frame() %>%
+    mutate(Method = rownames(.), 
+           Dataset = "0.40")
+)
+
+plot_main <- rmar_df %>%
+  mutate(TPR = TP / (TP + FN), 
+         FDR = FP / (TP + FP)
+         ) %>%
+  gather(Measure, Value, 7:8) %>%
+  mutate(Measure = factor(Measure, levels = c("TPR", "FDR")),
+         Method = factor(Method, levels = c("quantile", "beta", "manova", "mlm", "iForest", "mahdist", "ramr-IQR", "ramr-beta", "ramr-wbeta"))) %>%
+  ggplot(aes(x = Method, y = Value, fill = Method)) +
+    geom_bar(stat = "identity", position = position_dodge()) +
+  facet_grid(Dataset ~ Measure) +
+  scale_fill_manual(values = c("#E69F00", "#F0E442","cyan", "deepskyblue2", "deepskyblue4", "blue", "grey10", "grey50", "grey90")) +
+  theme_bw() +
+  theme(axis.text.x  = element_text(angle = 90, vjust = 0.5))
+
+png("figures/simulations_effectsize.png", res = 300, width = 2500, height = 1000)
+plot_main 
+dev.off()
+
+
+# Sample size ####
+## Simulate data a general dataset with only unique epimutations and subset ####
+set.seed(27)
+Nsamp <- seq(20, 90, 10)
+Nsim <- 10
+sim_sample_res <- lapply(Nsamp, function(N){
+  
+  lapply(seq_len(Nsim), function(i){
+    message("Iteration", i)
+    samps <- sample(colnames(mcols(sim.data040_samp)), N)
+    epimut_samp <- lapply(names(epi_parameters()), function(met){
+      epimutations_one_leave_out(simGset040_samp[, samps], method = met)
+    })
+    names(epimut_samp) <- names(epi_parameters())
+    
+    epimut_sampGR <- lapply(epimut_samp, function(x) {
+      GR <- makeGRangesFromDataFrame(x, keep.extra.columns = TRUE)
+      GR[seqnames(GR) != 0]
+    })
+    
+    ramr_samp <- lapply(c("IQR", "beta", "wbeta"), function(met){
+      subsamp <- sim.data040_samp
+      mcols(subsamp) <- mcols(sim.data040_samp)[, samps]
+      getAMR(subsamp, ramr.method = met, min.cpgs = 3,
+             merge.window = 1000, cores = 2, qval.cutoff = 1e-6)
+    })
+    
+    ramr_samp_def <- lapply(c("IQR", "beta", "wbeta"), function(met){
+      subsamp <- sim.data040_samp
+      mcols(subsamp) <- mcols(sim.data040_samp)[, samps]
+      getAMR(subsamp, ramr.method = met, min.cpgs = 3,
+             merge.window = 1000, cores = 2, qval.cutoff = 1e-3)
+    })
+    names(ramr_samp_def) <- paste(c("IQR", "rmar-beta", "wbeta"), "def")
+    
+    list(samples = samps, results = c(epimut_sampGR, ramr_samp, ramr_samp_def))
+  })
+})
+save(sim_sample_res, file = "results/simulations/sim_res_samplesize.Rdata")
+
+
+## Create tables
+sim_sample_tabs <- lapply(sim_sample_res, function(nsampL){
+  lapply(nsampL, function(iterL){
+    regs <- subset(amrs.unique_040, sample %in% iterL$samples)
+    names(iterL$res)[7:9] <- c("ramr-IQR", "ramr-beta", "ramr-wbeta")
+    vals <- sapply(iterL$res, getValues, epi_regs = regs,
+                   all_ranges = all.ranges)
+    
+    t(vals) %>% 
+      data.frame() %>%
+      mutate(Method = rownames(.)) %>%
+      mutate(ACC = (TPu + TN) / (TPu + TN + FP + FNu),
+             SP = TN / (FP + TN),
+             TPR = TPu / (TPu + FNu), 
+             FDR = FP / (TPu + FP)) %>%
+      as_tibble()
+  })
+})
+sim_sample_tabs_N <- lapply(sim_sample_tabs, Reduce, f = rbind)
+sim_sample_tabs_df <- Reduce(rbind, sim_sample_tabs_N) %>%
+  mutate(N = rep(Nsamp, sapply(sim_sample_tabs_N, nrow)))
+
+## epimutations
+
+sample_size_plot_epi <- sim_sample_tabs_df %>%
+  filter(Method %in% c("beta", "quantile", "mahdist", "mlm", "iForest", "manova")) %>%
+  select(Method, TPR, FDR, N) %>%
+  gather(Measure, Value, 2:3) %>%
+  mutate(Measure = factor(Measure, levels = c("TPR", "FDR"))) %>%
+  ggplot(aes(x = N, y = Value, color = Method)) +
+  geom_point() +
+  geom_smooth(span = 0.5) +
+  theme_bw() +
+  facet_grid(~ Measure)
+
+
+sample_size_plot_ramr <- sim_sample_tabs_df %>%
+  filter(Method %in% c("ramr-beta", "ramr-IQR", "ramr-wbeta")) %>%
+  select(Method, TPR, FDR, N) %>%
+  gather(Measure, Value, 2:3) %>%
+  mutate(Measure = factor(Measure, levels = c("TPR", "FDR"))) %>%
+  ggplot(aes(x = N, y = Value, color = Method)) +
+  geom_point() +
+  geom_smooth(span = 0.5) +
+  theme_bw() +
+  facet_grid(~ Measure)
+
+plot_grid(sample_size_plot_epi, sample_size_plot_ramr, nrow = 2)
+
+
+sim_sample_tabs_df %>%
+  mutate(TPR = SN) %>%
+  select(Method, TPR, FDR, N) %>%
+  gather(Measure, Value, 2:3) %>%
+  group_by(Method, Measure, N) %>%
+  summarize(m = median(Value)) %>%
+  spread(N, m) %>% 
+  data.frame()
+
+
+
+
+# Epimut. frequency ####
+### Subset input data to reigons that can have epimutations
+# data.ranges.epi <- subsetByOverlaps(data.ranges, all.ranges)
+
+### Only add non-unique epimutations
+simulate_non_unique <- function(N, diff, Nregs){
+  
+  epi_regs <- sample(all.ranges, 1000)
+  data.ranges.epi <- subsetByOverlaps(data.ranges, epi_regs)
+  nonunique <-
+    simulateAMR(data.ranges.epi, nsamples = N, 
+                regions.per.sample = Nregs, samples.per.region = N, min.cpgs = 3,
+                merge.window = 1000, dbeta = diff)
+   sim.data <-
+    simulateData(data.ranges.epi, nsamples = 100,
+                 amr.ranges = nonunique, cores = 2)
+   gset <- makeGenomicRatioSetFromMatrix(data.matrix(mcols(sim.data)))
+   
+  list(regs = nonunique, ramr_data = sim.data, epimut_data = gset)
+}
+
+set.seed(27)
+nonunique_sim <- lapply(2:10, simulate_non_unique, diff = 0.4, Nregs = 100)
+save(nonunique_sim, file = "results/simulations/nonunique_data.Rdata")
+
+## Run methods ####
+nonunique_sim_ramr <- lapply(nonunique_sim, function(iter){
+  lapply(c("IQR", "beta", "wbeta"), function(met){
+    getAMR(iter$ramr_data, ramr.method = met, min.cpgs = 3,
+           qval.cutoff = 1e-3,
+           merge.window = 1000, cores = 2)
+  })
+})
+save(nonunique_sim_ramr, file = "results/simulations/nonunique_ramr.Rdata")
+
+nonunique_sim_cc <- lapply(nonunique_sim, function(iter){
+  lapply(names(epi_parameters()), function(met){
+    gset <- iter$epimut_data
+    samps <-  unique(iter$regs$sample)
+    epimutations(gset[, samps], gset[, !colnames(gset) %in% samps], method = met)
+  })
+})
+save(nonunique_sim_cc, file = "results/simulations/nonunique_casecontrol.Rdata")
+
+nonunique_sim_loo <- lapply(nonunique_sim, function(iter){
+  lapply(names(epi_parameters()), function(met){
+    epimutations_one_leave_out(iter$epimut_data, method = met)
+  })
+})
+save(nonunique_sim_loo, file = "results/simulations/nonunique_loo.Rdata")
+
+
+processIter <- function(iter, sim){
+  
+  res <- sapply(iter, function(x) 
+    getValues(x, epi_regs =  sim$regs, 
+              all_ranges = getUniverse(sim$ramr_data, min.cpgs = 3, merge.window = 1000)
+))
+  res %>%
+    t() %>%
+    data.frame() %>%
+    mutate(Method = c("ramr-IQR", "ramr-beta", "ramr-wbeta")) %>%
+    select(Method, TP, FN, TN, FP)
+}
+
+nonunique_sim_ramr_l <- Map(processIter, nonunique_sim_ramr, nonunique_sim)
+nonunique_sim_ramr_df <- Reduce(rbind, nonunique_sim_ramr_l) %>%
+  mutate(N = rep(2:10, sapply(nonunique_sim_ramr_l, nrow)),
+         Mode = "LOO",
+         Package = "ramr")
+
+
+processIterEpimut <- function(iter, sim){
+  
+  res <- sapply(iter, function(x) {
+    regs <- makeGRangesFromDataFrame(x, keep.extra.columns = TRUE)
+    regs <- regs[seqnames(regs) != "0"]
+    getValues(regs, epi_regs =  sim$regs,
+              all_ranges = getUniverse(sim$ramr_data, min.cpgs = 3, merge.window = 1000)
+    )
+    })
+  res %>%
+    t() %>%
+    data.frame() %>%
+    mutate(Method = names(epi_parameters())) %>%
+    select(Method, TP, FN, TN, FP)
+}
+
+nonunique_sim_cc_l <- Map(processIterEpimut, nonunique_sim_cc, nonunique_sim)
+nonunique_sim_cc_df <- Reduce(rbind, nonunique_sim_cc_l) %>%
+  mutate(N = rep(2:10, sapply(nonunique_sim_cc_l, nrow)),
+         Mode = "Case-Control", 
+         Package = "epimutacions")
+
+nonunique_sim_loo_l <- Map(processIterEpimut, nonunique_sim_loo, nonunique_sim)
+nonunique_sim_loo_df <- Reduce(rbind, nonunique_sim_loo_l) %>%
+  mutate(N = rep(2:10, sapply(nonunique_sim_loo_l, nrow)),
+         Mode = "LOO",
+         Package = "epimutacions")
+
+nonunique_df <- rbind(nonunique_sim_ramr_df, nonunique_sim_cc_df, nonunique_sim_loo_df) %>%
+  mutate(TPR = TP / (TP + FN))
+
+plot_nonunique <- nonunique_df %>%
+  mutate(Method = factor(Method, levels = c("quantile", "beta", "manova", "mlm", "iForest", "mahdist", "ramr-IQR", "ramr-beta", "ramr-wbeta"))) %>%
+  ggplot(aes(x = N, y = TPR, color = Method)) +
+  geom_point() +
+  geom_line() +
+  scale_color_manual(values = c("#E69F00", "#F0E442","cyan", "deepskyblue2", "deepskyblue4", "blue", "grey10", "grey50", "grey90")) +
+  theme_bw() +
+  xlab("Epimut. Freq (%)") +
+  facet_grid(. ~ Package +  Mode  )
+
+
+png("figures/simulations_freq.png", res = 300, width = 2500, height = 1000)
+plot_nonunique 
+dev.off()
+
+# Time ####
+### Create new simulations with only unique epimutations in 10 samples
+amrs.time <- subset(amrs.unique_040, sample %in% paste0("sample", 1:10))
+
+## Simulate data ####
+set.seed(27)
+sim.data_time <-
+  simulateData(data.ranges, nsamples = 100,
+               amr.ranges = c(amrs.time, noise_040), cores = 2)
+save(sim.data_time, amrs.time, file = "results/simulations/sim.time_ramr_format.Rdata")
+
+
